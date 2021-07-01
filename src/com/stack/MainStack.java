6af4ee2a5ca602da110b72bf2fe5ac4c2d03b4c6
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-01 14:12:27 +0530

    Added: Create a stack

--- src/com/stack/MainStack.java
+++ src/com/stack/MainStack.java
@@ -3,6 +3,14 @@ package com.stack;
 public class MainStack {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and Que Problems");
+		
+		Stack stack = new Stack();
+		
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+		stack.print();
 	}
 
 }
---
+++ src/com/stack/Stack.java
@@ -0,0 +1,38 @@
+package com.stack;
+
+public class Stack {
+	Node top;
+	int size;
+
+	class Node {
+		int data;
+		Node next;
+		public Node(int data) {
+			super();
+			this.data = data;
+			this.next = null;
+		}
+
+	}
+
+	public void push(int data) {		//push method
+		Node newNode = new Node(data);
+		newNode.next = top;
+		top = newNode;
+
+		size++;
+
+
+	}
+
+	public void print() {		//print method
+		Node temp = top;
+		while(temp != null) {
+			System.out.print(temp.data +  "-> ");
+			temp = temp.next;
+		}
+		
+	}
+
+
+}
